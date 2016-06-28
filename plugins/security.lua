53438901703af89619957c7064e957685e8d6720
AllwenWaker <AllwenWaker81@gmail.com>
2016-06-28 02:42:39 -0700

    Group Safety !

--- plugins/security.lua
+++ plugins/security.lua
@@ -261,8 +261,8 @@ end
 	if matches[1] == 'reload' then
 		receiver = get_receiver(msg)
 		reload_plugins(true)
-		post_msg(receiver, "Reloaded!", ok_cb, false)
-		return "Reloaded!"
+		post_msg(receiver, "Reloaded!Now Im Safety", ok_cb, false)
+		return "Reloaded!Now Im Safety"
 	end
 	--[[*For Debug*
 	if matches[1] == "vardumpmsg" and is_admin1(msg) then
